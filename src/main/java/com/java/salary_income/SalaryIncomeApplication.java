275853181a0bae272969f8e2481de2888f76fc85
tetejiro <hy1733505@gmail.com>
2025-03-26 19:51:20 +0900

    first commit

---
+++ src/main/java/com/java/salary_income/SalaryIncomeApplication.java
@@ -0,0 +1,13 @@
+package com.java.salary_income;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryIncomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryIncomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/salary_income/SalaryIncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.salary_income;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryIncomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
